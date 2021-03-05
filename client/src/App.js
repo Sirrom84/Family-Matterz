b4a14d5ab2e5717a225b2fb1945b2f828c62f420
quinaiton <quinaiton@hotmail.com>
2021-03-05 13:14:09 -0800

    Adds top and Bottom Nav Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import { BottomNav } from './components/BottomNav';
+import { TopNav } from './components/TopNav';
 function App() {
   return (
     <div className='App'>
+      <TopNav></TopNav>
       <h1>Family Matterz</h1>
+      <BottomNav />
     </div>
   );
 }
