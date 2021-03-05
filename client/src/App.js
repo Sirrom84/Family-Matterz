ad7de20ff27b636533642fccf1173446fad16cf2
Sirrom84 <morrisrjc@gmail.com>
2021-03-05 16:45:19 -0700

    added npm dev dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import { BottomNav } from './components/BottomNav';
-import { TopNav } from './components/TopNav';
+import "./App.css";
+import {BottomNav} from "./components/BottomNav";
+import {TopNav} from "./components/TopNav";
 function App() {
-  return (
-    <div className='App'>
-      <TopNav></TopNav>
-      <h1>Family Matterz</h1>
-      <BottomNav />
-    </div>
-  );
+	return (
+		<div className="App">
+			<TopNav></TopNav>
+			<h1>Family Matterz</h1>
+			<BottomNav />
+		</div>
+	);
 }
 
 export default App;
