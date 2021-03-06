226e271da8009ae8ade3911d54a2dbb3be9b82b0
quinaiton <quinaiton@hotmail.com>
2021-03-06 14:44:05 -0800

    Adds Chore Chare Example

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import "./App.css";
-import {BottomNav} from "./components/BottomNav";
-import {TopNav} from "./components/TopNav";
+import './App.css';
+import { BottomNav } from './components/BottomNav';
+import { TopNav } from './components/TopNav';
+import { ChoreChart } from './components/ChoreChart';
 function App() {
-	return (
-		<div className="App">
-			<TopNav></TopNav>
-			<h1>Family Matterz</h1>
-			<BottomNav />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <TopNav></TopNav>
+      <h1>Family Matterz</h1>
+      <ChoreChart />
+      <BottomNav />
+    </div>
+  );
 }
 
 export default App;
