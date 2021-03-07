f5b6ff751311cdb6c66f10679c50b9e44a68ddf9
quinaiton <quinaiton@hotmail.com>
2021-03-07 13:21:48 -0800

    Centers content on home, Adds new Icons

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import 'primeicons/primeicons.css';
+import { ImBubbles2 } from 'react-icons/im';
 import './TopNav.css';
 
 export const TopNav = () => {
   return (
     <div>
       <div className='top-nav'>
-        <img src='./images/Logo.png' width='70' height='auto' alt='Logo' />
+        <img src='./images/Logo.png' width='50' height='auto' alt='Logo' />
         <h1 class>The Morris House</h1>
-        <i className='pi pi-envelope p-mr-2 p-text-secondary p-overlay-badge'></i>
+        <ImBubbles2 className='message' />
       </div>
     </div>
   );
