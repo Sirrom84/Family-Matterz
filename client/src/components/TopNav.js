4637e1df22484e5179d02d6916a4d3a38e6a1d6f
quinaiton <quinaiton@hotmail.com>
2021-03-08 13:49:15 -0800

    Adds New Logo And TimeLine to Home

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -6,8 +6,8 @@ export const TopNav = () => {
   return (
     <div>
       <div className='top-nav'>
-        <img src='./images/Logo.png' width='50' height='auto' alt='Logo' />
-        <h1 class>The Morris House</h1>
+        <img src='./images/FM-Logo.png' width='auto' height='70px' alt='Logo' />
+        <h2>The Morris House</h2>
         <ImBubbles2 className='message' />
       </div>
     </div>
