70e96df5c29db90256f2ebbcb1a9bec1d45caf26
Aaron Janes <Aaron.Janes@gmail.com>
2021-03-12 09:52:04 -0700

    Set up agenda

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,29 @@
 import "./App.css";
-<<<<<<< HEAD
 import { BottomNav } from "./components/BottomNav/BottomNav";
 import { TopNav } from "./components/TopNav/TopNav";
 import { ChoreChart } from "./components/ChoreChart/ChoreChart";
 import { TimelineHome } from "./components/TimeLine/TimeLineHome";
 import { GroceryList } from "./components/GroceryList/GroceryList";
-import MyCalender from "./components/Calender/Calender";
+import { Todo } from "./components/TaskApp/Todo";
+import Calender from "./components/Calender/Calender";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <TopNav />
-      <div className="grid-container">
-        <div className="two">
-          <TimelineHome />
-        </div>
-        <div className="three">
-          <ChoreChart />
-        </div>
-        <div className="four">
-          <GroceryList />
-          <div className="five">
-            <MyCalender />
-          </div>
-        </div>
+    <Router>
+      <div className="App">
+        <TopNav />
+        <Switch>
+          <Route path="/" exact component={TimelineHome} />
+          <Route path="/stats" component={ChoreChart} />
+          <Route path="/grocerylist" component={GroceryList} />
+          <Route path="/todolist" component={Todo} />
+          <Route path="/calender" component={Calender} />
+        </Switch>
+        <BottomNav />
       </div>
-      <BottomNav />
-    </div>
+    </Router>
   );
-=======
-import {BottomNav} from "./components/BottomNav/BottomNav";
-import {TopNav} from "./components/TopNav/TopNav";
-import {ChoreChart} from "./components/ChoreChart/ChoreChart";
-import {TimelineHome} from "./components/TimeLine/TimeLineHome";
-import {GroceryList} from "./components/GroceryList/GroceryList";
-import {Todo} from "./components/TaskApp/Todo";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
-function App() {
-	return (
-		<Router>
-			<div className="App">
-				<TopNav />
-				<Switch>
-					<Route path="/" exact component={TimelineHome} />
-					<Route path="/stats" component={ChoreChart} />
-					<Route path="/grocerylist" component={GroceryList} />
-					<Route path="/todolist" component={Todo} />
-				</Switch>
-				<BottomNav />
-			</div>
-		</Router>
-	);
->>>>>>> fe794d9b5779ae737603db5cf939299cd4bf8aae
 }
 
 export default App;
