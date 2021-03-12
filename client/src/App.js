fdfb21cd4cfafc0ecd3cd4943640d14e9bd919a5
quinaiton <quinaiton@hotmail.com>
2021-03-11 18:27:40 -0800

    Changes App.css to App.scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import "./App.css";
-import { BottomNav } from "./components/BottomNav/BottomNav";
-import { TopNav } from "./components/TopNav/TopNav";
-import { ChoreChart } from "./components/ChoreChart/ChoreChart";
-import { TimelineHome } from "./components/TimeLine/TimeLineHome";
-import { GroceryList } from "./components/GroceryList/GroceryList";
-import { Todo } from "./components/TaskApp/Todo";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import './App.scss';
+import { BottomNav } from './components/BottomNav/BottomNav';
+import { TopNav } from './components/TopNav/TopNav';
+import { ChoreChart } from './components/ChoreChart/ChoreChart';
+import { TimelineHome } from './components/TimeLine/TimeLineHome';
+import { GroceryList } from './components/GroceryList/GroceryList';
+import { Todo } from './components/TaskApp/Todo';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <TopNav />
         <Switch>
-          <Route path="/" exact component={TimelineHome} />
-          <Route path="/stats" component={ChoreChart} />
-          <Route path="/grocerylist" component={GroceryList} />
-          <Route path="/todolist" component={Todo} />
+          <Route path='/' exact component={TimelineHome} />
+          <Route path='/stats' component={ChoreChart} />
+          <Route path='/grocerylist' component={GroceryList} />
+          <Route path='/todolist' component={Todo} />
         </Switch>
         <BottomNav />
       </div>
