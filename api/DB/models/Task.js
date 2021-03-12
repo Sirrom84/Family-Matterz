679e0f2b2d18b266b36a3f239e94c82b3ff89061
quinaiton <quinaiton@hotmail.com>
2021-03-11 16:17:02 -0800

    Reconfigures models and adds images

---
+++ api/DB/models/Task.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  task: String,
+  dueDate: Date,
+  completed: { type: Boolean, default: false },
+  creator: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  family: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Family',
+    },
+  },
+  created: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
