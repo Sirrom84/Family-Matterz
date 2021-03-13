5608495597f17d4bd40fdce0b459bc1abf89bba9
Sirrom84 <morrisrjc@gmail.com>
2021-03-13 12:59:05 -0700

    fixed lock issue

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {TopNav} from "./components/TopNav/TopNav";
 import {ChoreChart} from "./components/ChoreChart/ChoreChart";
 import {TimelineHome} from "./components/TimeLine/TimeLineHome";
 import {GroceryList} from "./components/GroceryList/GroceryList";
-import {Todo} from "./components/TaskApp/Todo";
+import {Todo} from "./components/Todo/Todo";
 import {Survey} from "./components/Survey/Survey";
 import Calender from "./components/Calender/Calender";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
