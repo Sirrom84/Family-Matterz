eba45cd0d6ca1334ccfd24f18643c2c89f57fc9a
Sirrom84 <morrisrjc@gmail.com>
2021-03-13 12:42:15 -0700

    Pulling

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { BottomNav } from "./components/BottomNav/BottomNav";
-import { TopNav } from "./components/TopNav/TopNav";
-import { ChoreChart } from "./components/ChoreChart/ChoreChart";
-import { TimelineHome } from "./components/TimeLine/TimeLineHome";
-import { GroceryList } from "./components/GroceryList/GroceryList";
-import { Todo } from "./components/TaskApp/Todo";
-import Calender from "./components/Calender/Calender";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <TopNav />
-        <Switch>
-          <Route path="/" exact component={TimelineHome} />
-          <Route path="/stats" component={ChoreChart} />
-          <Route path="/grocerylist" component={GroceryList} />
-          <Route path="/todolist" component={Todo} />
-          <Route path="/calender" component={Calender} />
-        </Switch>
-        <BottomNav />
-      </div>
-    </Router>
-  );
-=======
 import "./App.scss";
 
 import {BottomNav} from "./components/BottomNav/BottomNav";
@@ -54,7 +26,6 @@ function App() {
 			</div>
 		</Router>
 	);
->>>>>>> 987a65456ffbd683fa50af3da7043e688eacb436
 }
 
 export default App;
