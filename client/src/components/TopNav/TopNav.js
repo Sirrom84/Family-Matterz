e35432601277bbd867f1528011462aa3c98fed3d
Sirrom84 <morrisrjc@gmail.com>
2021-03-13 12:21:35 -0700

    Avatar for task import

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { ImBubbles2 } from 'react-icons/im';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {ImBubbles2} from "react-icons/im";
+import {Link} from "react-router-dom";
+import {Avatar} from "primereact/avatar";
+import {AvatarGroup} from "primereact/avatargroup";
+import {Badge} from "primereact/badge";
 
-import './TopNav.scss';
+import "./TopNav.scss";
 
 export const TopNav = () => {
-  return (
-    <div>
-      <div className='top-nav'>
-        <img src='./images/FM-Logo.png' width='auto' height='70px' alt='Logo' />
-        <h2>The Morris House</h2>
-        <Link to='/chat'>
-          <ImBubbles2 className='message' />
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="top-nav">
+				<img src="./images/FM-Logo.png" width="auto" height="70px" alt="Logo" />
+				<h2>The Morris House</h2>
+				<Link to="/chat">
+					<ImBubbles2 className="message" />
+				</Link>
+			</div>
+		</div>
+	);
 };
