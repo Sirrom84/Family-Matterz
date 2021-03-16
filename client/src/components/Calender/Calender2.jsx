64a4e0f7b7c8e7c7718d1a2485e321bf308a62bb
Aaron Janes <Aaron.Janes@gmail.com>
2021-03-15 21:11:39 -0600

    custom hook for updating state

--- client/src/components/Calender/Calender2.jsx
+++ client/src/components/Calender/Calender2.jsx
@@ -1,33 +1,42 @@
-import React, { createRef } from "react";
+import React, { createRef, useState } from "react";
 import Scheduler, { Resource, Editing } from "devextreme-react/scheduler";
 import { useData, employees } from "./useData";
 import DataCell from "./DataCell";
 import ResourceCell from "./ResourceCell";
 import "./styles.css";
 import Button from "devextreme-react/button";
+
 const currentDate = new Date(2021, 2, 26, 1, 30);
 const groups = ["employeeID"];
 const views = ["month", "day", "workWeek", "agenda"];
 
 const schedulerRef = createRef();
 
-const addAppointment = () => {
-  schedulerRef.current.instance.addAppointment({
-    text: "Website Re-Design Plan",
-    startDate: new Date(2021, 3, 25, 9, 30),
-    endDate: new Date(2021, 3, 25, 11, 30),
-  });
-};
-
 const App = () => {
+  // brings data in from Mongo
   const data = useData();
+
+  // set up a local state object
+  const [appointments, setAppointments] = useState();
+
+  console.log("appointments :>> ", appointments);
+
+  // create a function to update state with the new appointment data
+  // how do I access the create appointment on click function ??
+
+  const addAppointment = () => {
+    setAppointments(prevAppointments => { ...prevAppointments, {text: "Website Re-Design Plan",
+    startDate: new Date(2016, 4, 25, 9, 30),
+    endDate: new Date(2016, 4, 25, 11, 30) )}
+  };
 
   return (
     <>
-      <Button text="Add" onClick={addAppointment} />
+      {/* make a button with a onlick handler ?  can i access the model  */}
+      {/* <Button text="Add" onClick={addAppointment} /> */}
       <Scheduler
         timeZone="America/Los_Angeles"
-        dataSource={data}
+        dataSource={appointments}
         dataCellComponent={DataCell}
         resourceCellComponent={ResourceCell}
         groups={groups}
