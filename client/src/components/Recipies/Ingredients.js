1406c9f107044b3ef0a7b5a22146698274969432
quinaiton <quinaiton@hotmail.com>
2021-03-17 11:21:06 -0700

    Fixing asyc Bug

--- client/src/components/Recipies/Ingredients.js
+++ client/src/components/Recipies/Ingredients.js
@@ -15,37 +15,44 @@ export const Ingredients = (props) => {
       .get(url)
       .then((res) => {
         setIngredients(res.data);
+        console.log('data retreived');
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const ingredientList = ingredients.extendedIngredients.map((item) => {
-    return <li>{item.originalString}</li>;
-  });
+  console.log(ingredients);
 
-  console.log();
+  if (ingredients !== null) {
+    const ingredientList = ingredients.extendedIngredients.map((item) => {
+      return <li>{item.originalString}</li>;
+    });
 
-  const instructions = ingredients.analyzedInstructions[0].steps.map((item) => {
-    return <li>{item.step}</li>;
-  });
-  return (
-    <div>
-      <div className='grid-container'>
-        <div className='title'>
-          <h1>{item.title}</h1>
-          <img thumbnail='true' src={item.image} alt={item.title} />
-        </div>
-        <div className='instructions'>
-          <h2>Instructions</h2>
-          <ol>{instructions}</ol>
-        </div>
-        <div className='ingredients'>
-          <h2>Ingredients</h2>
-          <ul>{ingredientList}</ul>
+    const instructions = ingredients.analyzedInstructions[0].steps.map(
+      (item) => {
+        return <li>{item.step}</li>;
+      }
+    );
+
+    return (
+      <div>
+        <div className='grid-container'>
+          <div className='title'>
+            <img roundedCircle src={item.image} alt={item.title} />
+            <h1>{item.title}</h1>
+            <hr />
+          </div>
+          <div className='instructions'>
+            <h2>Instructions</h2>
+            <ol>{instructions}</ol>
+          </div>
+          <div className='ingredients'>
+            <h2>Ingredients</h2>
+            <ul>{ingredientList}</ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
