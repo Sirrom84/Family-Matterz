90b33463b68c8ae246a480ad89922abf34e6514d
quinaiton <quinaiton@hotmail.com>
2021-03-17 19:25:49 -0700

    Fixes Nav Image

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -13,12 +13,7 @@ export const TopNav = () => {
       <div className='top-nav'>
         <img src='./images/FM-Logo.png' width='auto' height='70px' alt='Logo' />
         <h2>The Morris House</h2>
-        <Avatar
-          image='./images/dad.png'
-          className='avatar'
-          size='large'
-          shape='circle'
-        />
+        <Avatar image='./images/dad.png' size='large' shape='circle' />
       </div>
     </div>
   );
