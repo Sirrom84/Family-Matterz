99634dadc0b1b80242654f0f694bd957f3fd3224
Sirrom84 <morrisrjc@gmail.com>
2021-03-18 20:58:21 -0600

    messing around with styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,8 @@ import {GroceryList} from "./components/GroceryList/GroceryList";
 import {Todo} from "./components/Todo/Todo";
 import {Survey} from "./components/Survey/Survey";
 import {Recipes} from "./components/Recipies/Recipes";
-import {Ingredients} from "./components/Recipies/Ingredients";
 import Calender2 from "./components/Calender/Calender2";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Numbers from "./components/Numbers/Numbers";
-import Login from "./components/Login/Login";
 
 function App() {
 	return (
@@ -20,16 +17,13 @@ function App() {
 				<TopNav />
 				<Switch>
 					<Route path="/" exact component={TimelineHome} />
-					<Route path="/welcome" component={Login} />
 					<Route path="/stats" component={ChoreChart} />
 					<Route path="/grocerylist" component={GroceryList} />
 					<Route path="/todo" component={Todo} />
 					<Route path="/calender" component={Calender2} />
 					<Route path="/survey" component={Survey} />
 					<Route path="/todolist" component={Todo} />
-					<Route path="/recipes/:id/ingredients" component={Ingredients} />
 					<Route path="/recipes" component={Recipes} />
-					<Route path="/numbers" component={Numbers} />
 				</Switch>
 				<BottomNav />
 			</div>
---
+++ client/src/components/TimeLine/TimeLineHome.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import {Todo} from "../Todo/Todo";
+import "./TimelineHome.scss";
+
+export const TimelineHome = (props) => {
+	return (
+		<div className="grid-container">
+			<div className="one">
+				<h3>Calendar</h3>
+			</div>
+			<div className="two">
+				<h3>Todo</h3>
+			</div>
+			<div className="three">
+				<h3>Dinner Survey</h3>
+			</div>
+			<div className="four">
+				<h3>Grocery List</h3>
+			</div>
+			<div className="five">
+				<h3>Random</h3>
+			</div>
+		</div>
+	);
+};
