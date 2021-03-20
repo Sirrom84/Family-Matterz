bb22f18f8332e988e5a4bec10750edddcb716689
Sirrom84 <morrisrjc@gmail.com>
2021-03-19 21:25:36 -0600

    Styling

--- client/src/components/TimeLine/TimeLineHome.jsx
+++ client/src/components/TimeLine/TimeLineHome.jsx
@@ -10,41 +10,45 @@ import {Todo} from "../Todo/Todo";
 import Welcome from "./Welcome";
 import {Link} from "react-router-dom";
 import {Avatar} from "primereact/avatar";
+import TopNav from "../TopNav/TopNav";
 
 import "./TimelineHome.scss";
 
 export const TimelineHome = (props) => {
 	return (
-		<div className="grid-container">
-			<div className="one">
-				<Link to="/calendar" style={{textDecoration: "none"}}>
-					<div className="current">
-						<Welcome />
-					</div>
-				</Link>
+		<div>
+			<TopNav />
+			<div className="grid-container">
+				<div className="one">
+					<Link to="/calender" style={{textDecoration: "none"}}>
+						<div className="current">
+							<Welcome />
+						</div>
+					</Link>
 
-				<p className="user-home-text">
-					The Smith Family <br></br>Friday March 19, 2021
-				</p>
-				<img className="dash-log" src="/images/newGirl.png" />
-				{/* <Avatar
+					<p className="user-home-text">
+						The Smith Family <br></br>Friday March 19, 2021
+					</p>
+					<img className="dash-log" src="/images/newGirl.png" />
+					{/* <Avatar
 					image="/images/daughter.png"
 					className="task-avatar"
 					size="large"
 					shape="circle"
 				/> */}
-			</div>
-			<Link to="/todolist">
-				<div className="two"></div>
-			</Link>
+				</div>
+				<Link to="/todolist">
+					<div className="two"></div>
+				</Link>
 
-			<div className="three"></div>
-			<Link to="/grocerylist">
-				<div className="four"></div>
-			</Link>
-			<Link to="/recipes">
-				<div className="five"></div>
-			</Link>
+				<div className="three"></div>
+				<Link to="/grocerylist">
+					<div className="four"></div>
+				</Link>
+				<Link to="/recipes">
+					<div className="five"></div>
+				</Link>
+			</div>
 		</div>
 	);
 };
