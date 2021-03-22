7eb3bcf0ca1faad32942e989b8f7be18994728ee
Rob  Morris <morrisrjc@gmail.com>
2021-03-21 22:21:42 -0600

    styling in progress

--- client/src/components/TopNav/TopNav.jsx
+++ client/src/components/TopNav/TopNav.jsx
@@ -11,8 +11,14 @@ export default function TopNav() {
 	return (
 		<div>
 			<div className="top-nav">
-				<img src="./images/FM-Logo.png" width="auto" height="70px" alt="Logo" />
-				<h2>The Morris House</h2>
+				<img
+					className="Logo"
+					src="./images/LogoHome.svg"
+					width="auto"
+					height="55px"
+					alt="Logo"
+				/>
+				{/* <h2>Family Matterz</h2> */}
 				<Avatar image="./images/daughter.png" size="large" shape="circle" />
 			</div>
 		</div>
